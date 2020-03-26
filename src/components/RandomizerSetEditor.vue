86ee5d2ca6e3ea434a15cddc71a86b7ffa36f636
Frank Bauer <frank.bauer@fau.de>
2020-03-26 16:00:05 +0100

    Converted RandomizerSetEditor to ts

--- src/components/RandomizerSetEditor.vue
+++ src/components/RandomizerSetEditor.vue
@@ -1,67 +1,44 @@
 <template>
-  
-    <q-popup-proxy transition-show="flip-up" transition-hide="flip-down" @before-show="onShow">                    <!-- LineNumbers -->
-        <div class="q-pa-md"> 
-            <div class="row no-wrap q-pa-none"> 
-                <div class="text-overline">{{$t('RandomizerSetEditor.Caption', {nr:nr})}}</div>
-            </div>                  
-            <div class="q-pl-md" v-for="tag in tagSet.values" v-bind:key="tag.tag"> 
-               <q-input
-                        v-model="tag.value"
-                        :label="tag.tag"
-                />
+    <q-popup-proxy transition-show="flip-up" transition-hide="flip-down" @before-show="onShow">
+        <!-- LineNumbers -->
+        <div class="q-pa-md">
+            <div class="row no-wrap q-pa-none">
+                <div class="text-overline">{{ $t('RandomizerSetEditor.Caption', { nr: nr }) }}</div>
+            </div>
+            <div class="q-pl-md" v-for="tag in tagSet.values" v-bind:key="tag.tag">
+                <q-input v-model="tag.value" :label="tag.tag" />
             </div>
         </div>
-
-    </q-popup-proxy >
+    </q-popup-proxy>
 </template>
 
-<script>
-import Vue from 'vue'
-export default {
-    props:{
-        options:{
-            type:Object,
-            required:true
-        },
-        tagSet:{
-            type:Object,
-            required:true
-        },
-        nr:{
-            type:Number,
-            required:true
-        }
-    },
-    computed:{
-        tags(){
-            return this.tagSet.values.map(v => v.tag);
-        }
-    },
-    methods:{
-        onShow(o){
-            this.tagSet.values = this
-                                    .tagSet
-                                    .values
-                                    .filter(v => this
-                                                    .options
-                                                    .randomizer
-                                                    .knownTags
-                                                    .indexOf(v.tag)>=0
-                                            );
-            this.options.randomizer.knownTags.forEach(t => {
-                if (this.tagSet.values.find(v => (v.tag==t)) === undefined){
-                    this.tagSet.values.push({
-                        tag:t,
-                        value:''
-                    })
-                }
-            })
-        }
+<script lang="ts">
+import 'reflect-metadata'
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import { IRandomizerSet } from '../lib/ICodeBlocks'
+
+@Component
+export default class RandomizerSetEditor extends Vue {
+    @Prop({ required: true }) options!: any
+    @Prop({ required: true }) tagSet!: IRandomizerSet
+    @Prop({ required: true }) nr!: number
+
+    get tags(): string[] {
+        return this.tagSet.values.map(v => v.tag)
+    }
+
+    onShow(o) {
+        this.tagSet.values = this.tagSet.values.filter(v => this.options.randomizer.knownTags.indexOf(v.tag) >= 0)
+        this.options.randomizer.knownTags.forEach(t => {
+            if (this.tagSet.values.find(v => v.tag == t) === undefined) {
+                this.tagSet.values.push({
+                    tag: t,
+                    value: ''
+                })
+            }
+        })
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
