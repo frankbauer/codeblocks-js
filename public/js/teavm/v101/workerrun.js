c1a82bc4e82c2851d66fede0d3fb45c1e1777ffe
Frank Bauer <frank.bauer@fau.de>
2020-12-08 17:28:36 +0100

    new version of teavm, and worker

--- public/js/teavm/v101/workerrun.js
+++ public/js/teavm/v101/workerrun.js
@@ -1,36 +1,50 @@
 self.importScripts('worker/runtime.js')
 
 let didRun = false
+let $stderrBuffer = ''
+let $stdoutBuffer = ''
+
+const olog = console.log
+const oerr = console.error
+console.log = function(line){
+    olog("[WORKER]", ...arguments)    
+}
+
+console.error = function(ch){
+    oerr("[WORKER]", ...arguments)
+}
+
 function listener(event) {
     let request = event.data
     console.log('JAVA-WORKE-RUN-MSG', request)
     if (request.command == 'run' && !didRun) {
         didRun = true
-        var $stderrBuffer = ''
-        var $stdoutBuffer = ''
+        
+        const reqID = request.id
+        
 
-        // $rt_putStdoutCustom = function (ch) {
-        //     if (ch === 0xa) {
-        //         self.postMessage({ command: 'stdout', line: $stdoutBuffer, id: request.id })
-        //         $stdoutBuffer = ''
-        //     } else {
-        //         $stdoutBuffer += String.fromCharCode(ch)
-        //     }
-        // }
+        $rt_putStdoutCustom = function (ch) {
+            if (ch === 0xa) {
+                self.postMessage({ command: 'stdout', line: $stdoutBuffer, id: reqID })
+                $stdoutBuffer = ''
+            } else {
+                $stdoutBuffer += String.fromCharCode(ch)
+            }
+        }
 
-        // $rt_putStderrCustom = function (ch) {
-        //     if (ch === 0xa) {
-        //         self.postMessage({ command: 'stderr', line: $stderrBuffer, id: request.id })
-        //         $stderrBuffer = ''
-        //     } else {
-        //         $stderrBuffer += String.fromCharCode(ch)
-        //     }
-        // }
+        $rt_putStderrCustom = function (ch) {
+            if (ch === 0xa) {
+                self.postMessage({ command: 'stderr', line: $stderrBuffer, id: reqID })
+                $stderrBuffer = ''
+            } else {
+                $stderrBuffer += String.fromCharCode(ch)
+            }
+        }
 
         let blob = new Blob([request.code], { type: 'application/javascript' })
         let URLObject = URL.createObjectURL(blob)
         self.importScripts(URLObject)
-        self.postMessage({ command: 'run-finished-setup', id: request.id })
+        self.postMessage({ command: 'run-finished-setup', id: reqID })
 
         try {
             console.log('Starting')
@@ -46,20 +60,20 @@ function listener(event) {
         }
 
         if ($stderrBuffer != '') {
-            self.postMessage({ command: 'stderr', line: $stderrBuffer, id: request.id })
+            self.postMessage({ command: 'stderr', line: $stderrBuffer, id: reqID })
         }
         if ($stdoutBuffer != '') {
-            self.postMessage({ command: 'stdout', line: $stdoutBuffer, id: request.id })
+            self.postMessage({ command: 'stdout', line: $stdoutBuffer, id: reqID })
         }
 
-        // self.postMessage({ command: 'run-completed', id: request.id })
+        // self.postMessage({ command: 'run-completed', id: reqID })
 
-        // URLObject = undefined
-        // blob = undefined
+        URLObject = undefined
+        blob = undefined
         // $stderrBuffer = undefined
         // $stdoutBuffer = undefined
 
-        //self.removeEventListener('message', listener)
+        // self.removeEventListener('message', listener)
     }
     request = undefined
 }
