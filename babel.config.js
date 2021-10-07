fee5f103c360a11412e92ef5ab695ffe76cbb4ee
Frank Bauer <frank.bauer@fau.de>
2021-10-07 20:34:30 +0200

    Working Build System with latest versions for vue2

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: [
+        [
+            'transform-imports',
+            {
+                quasar: {
+                    transform: require('quasar/dist/babel-transforms/imports.js'),
+                    preventFullImport: true,
+                },
+            },
+        ],
+    ],
+    presets: ['@vue/cli-plugin-babel/preset'],
+}
