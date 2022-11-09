9cdbca2c5489a11290efee02decd04f3733f2d38
Frank Bauer <frank.bauer@fau.de>
2022-11-09 18:22:42 +0100

    Increased recursion limit for python

--- docs/examples/js/python/v102/pyWorker.js
+++ docs/examples/js/python/v102/pyWorker.js
@@ -99,7 +99,7 @@ async function listener(input) {
             pyodide.runPython(
                 `import sys
 import io
-sys.setrecursionlimit(200)
+sys.setrecursionlimit(1000)
 sys.stdout = io.StringIO()`,
                 globals
             )
