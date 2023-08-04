6641bf07369204dedb455eb1af4b88f764c2002f
Frank Bauer <frank.bauer@fau.de>
2023-08-04 15:41:37 +0200

    Migrated `ErrorTip` to Composite API

--- src/components/ErrorTip.vue
+++ src/components/ErrorTip.vue
@@ -20,55 +20,72 @@
 </template>
 
 <script lang="ts">
-import 'reflect-metadata'
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import { ErrorSeverity, ICompilerErrorDescription } from '@/lib/ICompilerRegistry'
+import { computed, ComputedRef, defineComponent, PropType } from 'vue'
 
-@Component
-export default class ErrorTip extends Vue {
-    @Prop() errors!: ICompilerErrorDescription
-    @Prop() severity!: ErrorSeverity
-
-    get severityClass(): string {
-        return this.classForSeverity(this.severity)
-    }
-    get severityIcon(): string {
-        return this.iconForSeverity(this.severity)
-    }
-
-    classForSeverity(s: ErrorSeverity): string {
-        if (s == ErrorSeverity.Error) {
-            return 'gutter-error'
+export default defineComponent({
+    name: 'ErrorTip',
+    components: {},
+    props: {
+        errors: { type: Array as PropType<ICompilerErrorDescription[]>, required: true },
+        severity: { type: Number as PropType<ErrorSeverity>, required: true },
+    },
+    setup(props, context) {
+        function classForSeverity(s: ErrorSeverity): string {
+            if (s == ErrorSeverity.Error) {
+                return 'gutter-error'
+            }
+            return 'gutter-warning'
         }
-        return 'gutter-warning'
-    }
-    iconForSeverity(s: ErrorSeverity): string {
-        if (s == ErrorSeverity.Error) {
-            return 'report'
+
+        function iconForSeverity(s: ErrorSeverity): string {
+            if (s == ErrorSeverity.Error) {
+                return 'report'
+            }
+            return 'warning'
         }
-        return 'warning'
-    }
-}
+
+        const severityClass: ComputedRef<string> = computed(() => {
+            return classForSeverity(props.severity)
+        })
+
+        const severityIcon: ComputedRef<string> = computed(() => {
+            return iconForSeverity(props.severity)
+        })
+
+        return {
+            classForSeverity,
+            iconForSeverity,
+            severityClass,
+            severityIcon,
+        }
+    },
+})
 </script>
 
 <style lang="sass">
 .mainTipIcon
     margin-top: -1px
+
 .tiplist
     font-size: 0.9rem
     list-style-type: none
     padding-left: 0px !important
     max-width: 480px
     min-width: 300px
+
     li:first-of-type
         padding: 0px 0px 0px 0px
+
     li
         padding: 10px 0px 0px 0px
         margin: 0px 0px 0px 0px
+
         .tipicon
             display: inline
             color: white !important
             vertical-align: top
+
         .tipper
             vertical-align: top
             font-family: monospace
