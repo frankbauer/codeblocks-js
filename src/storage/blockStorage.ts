c643b83d6150d19ae9b87f1ac16199c17cca4a60
Frank Bauer <frank.bauer@fau.de>
2025-02-13 16:51:31 +0100

    Change return type of getBlock in blockStorage

--- src/storage/blockStorage.ts
+++ src/storage/blockStorage.ts
@@ -6,21 +6,23 @@ const storage: Ref<UnwrapNestedRefs<MainBlock>>[] = []
 
 export function storeBlock(data: IAppSettings) {
     storage.push(ref(new MainBlock(data)))
-    return { appID:storage.length - 1 }
+    return { appID: storage.length - 1 }
 }
 
 export type BlockStorageType = ReturnType<typeof useBlockStorage>
 
 export const useBlockStorage = (appID: number) => {
     const appInfo = computed(() => {
-        console.log('appInfo', appID,storage[appID].value)
+        console.log('appInfo', appID, storage[appID].value)
         return storage[appID].value
     })
 
     const blockIDs = computed(() => appInfo.value.blocks.map((v) => v.uuid))
 
-    function getBlock(id: string): ComputedRef<UnwrapRef<BlockData>> {
-        return computed(() => appInfo.value.blocks.find((v) => v.uuid === id)!)
+    function getBlock(id: string): ComputedRef<BlockData> {
+        return computed(
+            () => appInfo.value.blocks.find((v) => v.uuid === id)!
+        ) as ComputedRef<BlockData>
     }
 
     return {
