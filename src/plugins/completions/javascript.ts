d3f324b4d3918f057a9879e0d90f7b9f9a5bfdd6
Frank Bauer <frank.bauer@fau.de>
2026-05-18 23:16:13 +0200

    fixed java and javaScript completions

--- src/plugins/completions/javascript.ts
+++ src/plugins/completions/javascript.ts
@@ -39,40 +39,14 @@ const jsSnippets: CompletionItem[] = [
         apply: 'for (let i = 0; i < length; i++) {\n    \n}',
     },
     {
-        label: 'main',
+        label: 'setupDOM',
         type: 'snippet',
-        detail: 'function main(args) { }',
-        apply: 'function main(args) {\n    \n}',
+        detail: 'function setupDOM() { }',
+        apply: 'function setupDOM() {\n    \n}',
     },
 ]
 
-const jsRuntimeAPI: CompletionItem[] = [
-    {
-        label: 'CodeBlocks.postResult(obj)',
-        type: 'function',
-        info: 'Send structured result data to the output parser',
-    },
-    {
-        label: 'CodeBlocks.postMessage(cmd, id, data)',
-        type: 'function',
-        info: 'Send a worker message to DOM-side handlers',
-    },
-    {
-        label: 'CodeBlocks.startReceivingEvents(handler)',
-        type: 'function',
-        info: 'Register a handler for messages from DOM code',
-    },
-    {
-        label: 'CodeBlocks.stopReceivingEvents()',
-        type: 'function',
-        info: 'Stop receiving DOM-side messages',
-    },
-    {
-        label: 'process.exit(code)',
-        type: 'function',
-        info: 'Terminate keepAlive sessions',
-    },
-]
+const jsRuntimeAPI: CompletionItem[] = []
 
 const jsBaseCompletions = [...jsKeywords, ...jsSnippets]
 
